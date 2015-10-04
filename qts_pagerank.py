876b127c20bd2e6d67b3ace2db2a16c4d76792bf
Yan <heyan4869@gmail.com>
2015-10-04 16:13:23 -0400

    Regular update

--- qts_pagerank.py
+++ qts_pagerank.py
@@ -15,6 +15,37 @@ import scipy.spatial.distance as distance
 
 #################################################################
 #
+#   function matrix_transfer():
+#       get the transition matrix M
+#
+#################################################################
+
+def matrix_transfer():
+    trans_txt_path = "hw3-resources/transition.txt"
+    trans_txt = open(trans_txt_path, 'r')
+    row_list = []
+    col_list = []
+    data_list = []
+    outer_count = {}
+    for line in trans_txt:
+        ele_tuple = line.split(' ')
+        row_list.append(int(ele_tuple[0]) - 1)
+        col_list.append(int(ele_tuple[1]) - 1)
+        count = outer_count.get(int(ele_tuple[0]) - 1, 0)
+        count += 1
+        outer_count[int(ele_tuple[0]) - 1] = count
+    size = max(max(row_list), max(col_list)) + 1
+    for idx in row_list:
+        data_list.append(1.0 / outer_count[idx])
+    trans_coo_mtx = sparse.coo_matrix((data_list, (row_list, col_list)), shape=(size, size), dtype=np.float)
+    print '\n' + "Transition matrix transfer finished." + '\n'
+    # trans_mtx has been transposed
+    trans_mtx = trans_coo_mtx.tocsr().transpose()
+    return trans_mtx
+
+
+#################################################################
+#
 #   function vector_transfer():
 #       construct topic-specific teleportation vector p_t
 #
@@ -23,15 +54,20 @@ import scipy.spatial.distance as distance
 def vector_transfer():
     topic_txt_path = "hw3-resources/doc-topics.txt"
     topic_txt = open(topic_txt_path, 'r')
-    topic_count = {}
-    # for line in topic_txt:
-    #     ele_tuple = line.split(' ')
-    #     count = topic_count.get(int(ele_tuple[1]), 0)
-    #     count += 1
-    #     topic_count[int(ele_tuple[1])] = count
-    # tstele_vec = []
-    # for idx in range(1, len(topic_count)):
-    #     tstele_vec.append(1.0 / topic_count[idx])
+    row_list = []
+    col_list = []
+    data_list = []
+    for line in topic_txt:
+        ele_tuple = line.split(' ')
+        row_list.append(int(ele_tuple[1]) - 1)
+        col_list.append(int(ele_tuple[0]) - 1)
+        data_list.append(1)
+    row_size = max(row_list) + 1
+    col_size = max(col_list) + 1
+    topic_coo_mtx = sparse.coo_matrix((data_list, (row_list, col_list)), shape=(row_size, col_size), dtype=np.float)
+    # count_in_topic = topic_coo_mtx.sum(axis=1)
+    topic_tele_mtx = topic_coo_mtx.toarray() / topic_coo_mtx.toarray().sum(axis=1, keepdims=True)
+    return topic_tele_mtx
 
 
 #################################################################
@@ -42,7 +78,7 @@ def vector_transfer():
 #################################################################
 
 def main():
-    print "hello"
+    vector_transfer()
 
 
 # use this line to execute the main function
